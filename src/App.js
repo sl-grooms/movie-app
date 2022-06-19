a37e51da229231552c4f49c1a9ae5fd78b187a15
samantha grooms <samantha@noventum.us>
2022-06-19 08:05:14 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import MovieList from "./components/MovieList.js";
 
-function App() {
+const App = () => {
+  const [movies, setMovies] = useState([]);
+
+  const getMovieRequest = async () => {
+    const url = "http://www.omdbapi.com/?s=star wars&apikey=c4bc58ec"
+
+    const response = await fetch(url);
+
+    const responseJson = await response.json();
+
+    console.log(responseJson);
+    setMovies(responseJson.Search);
+  };
+
+  useEffect(()=>{
+    getMovieRequest();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='container-fluid movie-app'>
+        <div className='row'>
+          <MovieList movies={movies} />
+        </div>
+      </div>
   );
-}
+};
 
 export default App;
